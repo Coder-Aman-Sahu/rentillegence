aaf5172f91b11d5e59c8851fe56bb121809674e1
SahuAman03 <sahuaman212@gmail.com>
2025-09-20 15:44:21 +0530

    review feature added

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ const ejsMate=require("ejs-mate");
 const MONGO_URL="mongodb://127.0.0.1:27017/wanderlust";
 const wrapAsync = require("./utils/wrapAsync.js");
 const ExpressError = require("./utils/ExpressError.js"); 
+const {listingSchema,reviewSchema} = require("./schema.js");
+const Review = require("./models/review.js");
 
 main()
   .then(() =>{
@@ -29,6 +31,28 @@ app.use(express.static(path.join(__dirname,"/public")));
 app.get("/",(req,res)=>{
     res.send("Hello World");
 })
+
+const validateListing = (req,res,next) =>{
+    let {error}=listingSchema.validate(req.body);
+  
+    if(error){
+        let errMsg=error.details.map(el => el.message).join(",");
+        throw new ExpressError(400,errMsg);
+    }else{
+        next();
+    }
+};
+
+const validateReview = (req,res,next) =>{
+    let {error}=reviewSchema.validate(req.body);
+  
+    if(error){
+        let errMsg=error.details.map(el => el.message).join(",");
+        throw new ExpressError(400,errMsg);
+    }else{
+        next();
+    }
+};
 //Index Route
 app.get("/listings",wrapAsync(async (req,res)=>{
     const allListings = await Listing.find({});
@@ -43,13 +67,13 @@ app.get("/listings/new",(req,res)=>{
 //Show Route
 app.get("/listings/:id",wrapAsync(async(req,res)=>{
     let {id}=req.params;
-    const listing = await Listing.findById(id);
+    const listing = await Listing.findById(id).populate("reviews");
     res.render("listings/show.ejs",{listing});
 }));
 
 //Create Route
-app.post("/listings",wrapAsync(async (req,res,next)=>{
-    if(!req.body.listing) throw new ExpressError(400,"Invalid Listing Data");
+app.post("/listings",validateListing,wrapAsync(async (req,res,next)=>{
+
     const newListing = new Listing(req.body.listing);
     await newListing.save();
     res.redirect("/listings");
@@ -64,8 +88,8 @@ app.get("/listings/:id/edit",wrapAsync(async(req,res)=>{
 }));
 
 //Update Route
-app.put("/listings/:id",wrapAsync(async(req,res)=>{
-    if(!req.body.listing) throw new ExpressError(400,"Invalid Listing Data");
+app.put("/listings/:id",validateListing,wrapAsync(async(req,res)=>{
+    
     let {id}=req.params;
     await Listing.findByIdAndUpdate(id,{...req.body.listing});
     res.redirect(`/listings/${id}`);
@@ -79,6 +103,27 @@ app.delete("/listings/:id",wrapAsync(async(req,res)=>{
     console.log(deletedListing);
     res.redirect("/listings");
 }));
+
+//Reviews
+//Post Review Route
+app.post("/listings/:id/reviews", validateReview, wrapAsync(async (req, res) => {
+    let listing = await Listing.findById(req.params.id);
+    let newReview = new Review(req.body.review);
+    listing.reviews.push(newReview);
+    await newReview.save();
+    await listing.save();
+    console.log("new review saved");
+    res.redirect(`/listings/${listing._id}`);
+}));
+
+//Delete Review Route
+app.delete("/listings/:id/reviews/:reviewId", wrapAsync(async (req, res) => {
+    let { id, reviewId } = req.params;
+    await Listing.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
+    await Review.findByIdAndDelete(reviewId);
+    res.redirect(`/listings/${id}`);
+}));
+
 // app.get("/testListing",async (req,res)=>{
 //     let sampleListing=new Listing({
 //         title:"My New Villa",
@@ -98,8 +143,8 @@ app.use((req, res, next) => {
 
 app.use((err,req,res,next)=>{
     let {statusCode=500,message="Something went wrong!"}=err;
-
-    res.status(statusCode).send(message);
+    res.status(statusCode).render("error.ejs",{message});
+    //res.status(statusCode).send(message);
 });
 
 app.listen(8080,() =>{
